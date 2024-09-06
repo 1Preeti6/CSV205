1fccac79b00ae2399e6a6cb680aa308948f5d178
Preeti <preetilakhera089@gmail.com>
2024-09-06 23:09:19 +0530

    first commit

---
+++ scripts/check-disk-usage.js
@@ -0,0 +1,13 @@
+const disk = require('diskusage');
+
+disk.check('/', function(err, info) {
+  if (err) {
+    console.log(err);
+  } else {
+    const usedPercentage = (info.total - info.free) / info.total * 100;
+    console.log(`Disk usage: ${usedPercentage.toFixed(2)}%`);
+    if (usedPercentage > 90) {
+      console.log('Warning: Disk usage is above 90%!');
+    }
+  }
+});
